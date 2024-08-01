bf579112f8625d444abac5fe026d9e89ce64cdab
mehedi-66 <19303001@iubat.edu>
2024-08-01 10:17:21 +0600

    Product Insert into database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,11 +11,13 @@ class ProductController extends Controller
     // [httpGet]
     public function show()
     {
-        $products = Product::all();
-        echo '<pre>';
-        print_r($products->toArray());
+        $products = Product::where('action_type', '!=', 'DELETE')
+                            ->orderBy('product_id', 'desc')
+                            ->get();
+        
+        $data = compact('products');
 
-        //return view('product.productlist');
+        return view('product.productlist')->with($data);
     }
 
     // [httpGet]
@@ -33,8 +35,36 @@ class ProductController extends Controller
                 'product_name' => 'required'
             ]
         );
-        echo "<pre>";
-        print_r($request->all());
-        //return view('product.productlist');
+        // echo "<pre>";
+        // print_r($request->all());
+        $product = new Product();
+
+        $product->registration_date = $request['registration_date'];  
+        $product->product_name = $request['product_name'];  
+        $product->product_code = $request['product_code'] ; 
+        $product->product_unit_type = $request['product_unit_type'];  
+        $product->product_unit_price = $request['product_unit_price'] ; 
+        $product->product_actual_price = $request['product_actual_price'] ;
+        $product->product_unit_price_c = $request['product_unit_price_c'];  
+        $product->product_net_price = $request['product_net_price'];  
+        $product->atv_rate = $request['atv_rate'];  
+        $product->material_description = $request['material_description'];  
+        $product->product_packing = $request['product_packing'];  
+        $product->import_information = $request['import_information'];  
+        $product->h_s_code = $request['h_s_code'];
+        $product->low_stock_alert = $request['low_stock_alert'];  
+        $product->product_description = $request['product_description'];  
+        $product->product_generic = $request['product_generic'];
+        $product->product_category = $request['product_category'] ; 
+        $product->product_brand = $request['product_brand'];
+        $product->product_grouping = $request['product_grouping'];  
+        $product->product_image = $request['product_image'];
+        $product->action_type = 'INSERT';
+        $product->user_id = 'sys-user';
+        $product->action_date = now();
+
+        $product->save();
+
+        return redirect('/product/list');
     }
 }
--- resources/views/product/productlist.blade.php
+++ resources/views/product/productlist.blade.php
@@ -7,9 +7,33 @@
 
 @section('main-section')
     <!-- START View Content Here -->
-
+      
         <h5>Products</h5>
-        <p>This is Product List Page</p>
+
+       
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>#SL</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Created</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($products as  $prod)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{$prod->product_name}}</td>
+                    <td>{{$prod->product_unit_price}}</td>
+                    <td>{{$prod->action_date}}</td>
+                    <td>Edit/Delete</td>
+                </tr>
+               @endforeach
+            </tbody>
+        </table>
+   
 
     <!-- END View Content Here -->
 @endsection
