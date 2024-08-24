9693b389c9977360a6f0c34149bb67c5f386e4dd
mehedi-66 <19303001@iubat.edu>
2024-08-24 11:53:04 +0600

    Batch functional work

--- resources/views/batch/addbatch.blade.php
+++ resources/views/batch/addbatch.blade.php
@@ -35,13 +35,34 @@
                         </span>
                     </div>
                     <div class="form-group col-md-6">
+                        <label for="production_date">Production Date <span class="text-danger"><b>*</b></span></label>
+                        <input id="production_date" type="date" name="production_date"
+                            value="{{ old('production_date', $batch->production_date) }}" class="form-control"
+                            id="production_date">
+                        <span class="text-danger">
+                            @error('production_date')
+                                {{ $message }}
+                            @enderror
+                        </span>
+                    </div>
+                    <div class="form-group col-md-6">
                         <label for="product_id"> Product Name <span class="text-danger"><b>*</b></span></label>
                         <select id="products" name="product_id" class="form-control">
                             <option value="" selected="">Select</option>
-                          
+
                         </select>
                         <span class="text-danger">
                             @error('product_id')
+                                {{ $message }}
+                            @enderror
+                        </span>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="expire_date">Expire Date <span class="text-danger"><b>*</b></span></label>
+                        <input id="expire_date" type="date" name="expire_date"
+                            value="{{ old('expire_date', $batch->expire_date) }}" class="form-control" id="expire_date">
+                        <span class="text-danger">
+                            @error('expire_date')
                                 {{ $message }}
                             @enderror
                         </span>
@@ -57,48 +78,6 @@
                             @enderror
                         </span>
                     </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="batch_title">Batch Title <span class="text-danger"><b>*</b></span></label>
-                        <input id="batch_title" type="text" name="batch_title" value="{{ old('batch_title', $batch->batch_title) }}"
-                            class="form-control" id="batch_title">
-                        <span class="text-danger">
-                            @error('batch_title')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="batch_no">Batch No <span class="text-danger"><b>*</b></span></label>
-                        <input id="batch_no" type="text" name="batch_no" value="{{ old('batch_no', $batch->batch_no) }}"
-                            class="form-control" id="batch_no">
-                        <span class="text-danger">
-                            @error('batch_no')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="production_date">Production Date <span class="text-danger"><b>*</b></span></label>
-                        <input type="date" name="production_date"
-                            value="{{ old('production_date', $batch->production_date) }}" class="form-control"
-                            id="production_date">
-                        <span class="text-danger">
-                            @error('production_date')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="expire_date">Expire Date <span class="text-danger"><b>*</b></span></label>
-                        <input type="date" name="expire_date" value="{{ old('expire_date', $batch->expire_date) }}"
-                            class="form-control" id="expire_date">
-                        <span class="text-danger">
-                            @error('expire_date')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
                     <div class="form-group col-md-6">
                         <label for="batch_packing">Batch Packing </label>
                         <input id="batch_packing" type="number" name="batch_packing" min="0"
@@ -110,9 +89,20 @@
                             @enderror
                         </span>
                     </div>
+
+                    <div class="form-group col-md-6">
+                        <label for="batch_title">Batch Title <span class="text-danger"><b>*</b></span></label>
+                        <input id="batch_title" type="text" name="batch_title"
+                            value="{{ old('batch_title', $batch->batch_title) }}" class="form-control" id="batch_title">
+                        <span class="text-danger">
+                            @error('batch_title')
+                                {{ $message }}
+                            @enderror
+                        </span>
+                    </div>
                     <div class="form-group col-md-6">
                         <label for="remark">Remark</label>
-                        <input type="text" name="remark" value="{{ old('remark', $batch->remark) }}"
+                        <input id="remark" type="text" name="remark" value="{{ old('remark', $batch->remark) }}"
                             class="form-control" id="remark">
                         <span class="text-danger">
                             @error('remark')
@@ -121,9 +111,20 @@
                         </span>
                     </div>
                     <div class="form-group col-md-6">
+                        <label for="batch_no">Batch No <span class="text-danger"><b>*</b></span></label>
+                        <input id="batch_no" type="text" name="batch_no"
+                            value="{{ old('batch_no', $batch->batch_no) }}" class="form-control" id="batch_no">
+                        <span class="text-danger">
+                            @error('batch_no')
+                                {{ $message }}
+                            @enderror
+                        </span>
+                    </div>
+
+                    <div class="form-group col-md-6">
                         <label for="import_info">Import Info</label>
-                        <input id="import_info" type="text" name="import_info" value="{{ old('import_info', $batch->import_info) }}"
-                            class="form-control" id="import_info">
+                        <input id="import_info" type="text" name="import_info"
+                            value="{{ old('import_info', $batch->import_info) }}" class="form-control" id="import_info">
                         <span class="text-danger">
                             @error('import_info')
                                 {{ $message }}
@@ -148,23 +149,24 @@
         <button type="submit" class="btn btn-primary">Save</button>
     </form>
     <script type="text/javascript">
-        
         let productList = [];
         let customerList = [];
+        let batchList = [];
 
         $(document).ready(function() {
-             $.ajax({
+            $.ajax({
                 url: "{{ url('/customer/getList') }}",
                 method: 'GET',
                 success: function(data) {
                     //console.log(data);
                     customerList = data;
                     $.each(data, function(key, customer) {
-                        $('#customers').append('<option value="' + customer.customer_id + '">' + customer
+                        $('#customers').append('<option value="' + customer.customer_id + '">' +
+                            customer
                             .customer_name + '</option>');
                     });
                 }
-          
+
             });
             $.ajax({
                 url: "{{ url('/product/getList') }}",
@@ -173,7 +175,8 @@
                     //console.log(data);
                     productList = data;
                     $.each(data, function(key, customer) {
-                        $('#products').append('<option value="' + customer.product_id + '">' + customer
+                        $('#products').append('<option value="' + customer.product_id + '">' +
+                            customer
                             .product_name + '</option>');
                     });
                 }
@@ -194,14 +197,26 @@
 
                 if (productId) {
                     $.ajax({
-                        url: "{{ url('/batch/getBatchByProductId') }}/" + productId, 
+                        url: "{{ url('/batch/getBatchByProductId') }}/" + productId,
                         type: "GET",
                         dataType: "json",
                         success: function(data) {
+                            batchList = data;
+
                             $('#existingBatchItems').empty();
-                            $('#existingBatchItems').append('<option value="">Existing Batch List ('+data.length+')</option>');
+                            $('#existingBatchItems').append(
+                                '<option value="">Existing Batch List (' + data.length +
+                                ')</option>');
                             $.each(data, function(key, item) {
-                                $('#existingBatchItems').append('<option value="'+item.batch_id+'">'+item.batch_title+'</option>');
+                                let customer = customerList.find(f => f.customer_id ==
+                                    item.customer_id);
+
+                                $('#existingBatchItems').append('<option value="' + item
+                                    .batch_id + '">' +
+                                    item.batch_no + ', ' + item.production_date +
+                                    ', ' + item.expire_date + ', ' +
+                                    customer.customer_name + ', ' + item.remark +
+                                    '</option>');
                             });
                         }
                     });
@@ -209,6 +224,26 @@
                     $('#existingBatchItems').empty();
                     $('#existingBatchItems').append('<option value="">Existing Batch List</option>');
                 }
+            });
+
+            $('#existingBatchItems').on('change', function() {
+                var batchId = $(this).val();
+                //debugger;
+                if (batchId) {
+
+                    let batchProduct = batchList.find(f => f.batch_id == batchId);
+                    //console.log(batchProduct);
+
+                    $('#production_date').val(batchProduct.production_date);
+                    $('#expire_date').val(batchProduct.expire_date);
+                    $('#batch_no').val(batchProduct.batch_no);
+                    $('#batch_packing').val(batchProduct.batch_packing);
+                    $('#import_info').val(batchProduct.import_info);
+                    $('#remark').val(batchProduct.remark);
+                }
+
+
+
             });
         });
     </script>
